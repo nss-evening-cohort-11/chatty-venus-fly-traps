8105c1a165365d8099ca93095aa371c8d93d2e0e
John Fleming <johndeclanfleming@gmail.com>
2020-03-11 18:55:45 -0500

    wip

---
+++ src/javascripts/helper/data/userData.js
@@ -0,0 +1,11 @@
+const users = [
+  { id: 'user1', name: 'Zoe' },
+  { id: 'user2', name: 'John' },
+  { id: 'user3', name: 'Liza' },
+  { id: 'user4', name: 'Michele' },
+  { id: 'user5', name: 'Joey' },
+];
+
+const getUsers = () => users;
+
+export default { getUsers };
