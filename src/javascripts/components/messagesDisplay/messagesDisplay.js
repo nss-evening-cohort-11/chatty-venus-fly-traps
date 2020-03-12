123ec58ec25d6f61330b2a53bb39707f2e5cd09f
Joey Petrone <joey.petrone@gmail.com>
2020-03-11 20:42:08 -0500

    Resolved delete message error

--- src/javascripts/components/messagesDisplay/messagesDisplay.js
+++ src/javascripts/components/messagesDisplay/messagesDisplay.js
@@ -8,7 +8,7 @@ const messageBuilder = () => {
   messageArray.forEach((message) => {
     domString += `<div class="alert alert-${message.color} alert-dismissible fade show" role="alert">`;
     domString += `  <strong>${message.name}</strong> <p>${message.message}</p>`;
-    domString += `  <button id=${message.id} type="button" class="close" data-dismiss="alert" aria-label="Close">`;
+    domString += `  <button id="${message.id}-btn" type="button" class="close" data-dismiss="alert" aria-label="Close">`;
     domString += '    <span aria-hidden="true">&times;</span>';
     domString += '  </button>';
     domString += '</div>';
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,13 +1,23 @@
 import messagesDisplay from './components/messagesDisplay/messagesDisplay';
 import users from './components/users/users';
-import deleteMessage from './components/delete_message/delete_message';
-import 'bootstrap';
+import messageData from './helper/data/messageData';
 import '../styles/main.scss';
+import 'bootstrap';
+
+
+const deleteMessage = (e) => {
+  const allMessages = messageData.getMessages();
+  const messageId = e.target.closest('button').id;
+  console.error('messages array before delete', allMessages);
+  const messagePosition = allMessages.findIndex((p) => p.id === messageId);
+  allMessages.splice(messagePosition, 1);
+  console.error('messages array after delete', allMessages);
+};
 
 const init = () => {
   users.printUsers();
   messagesDisplay.messageBuilder();
-  $('#message-container').on('click', deleteMessage.deleteMessage());
+  $('#message-container').on('click', deleteMessage);
 };
 
 init();
