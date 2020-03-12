7a5e8ec04ba40f4bb5916118445785cadbf112a7
Liza Star <lizadstar18@gmail.com>
2020-03-11 20:45:39 -0500

    wip

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -4,6 +4,5 @@ import messagesData from './helper/data/messageData';
 const addMessage = () => {
   const messageInput = $('#message-input').val();
   messagesData.setMessages(messageInput);
-  console.error('');
 };
 $('#body').on('keypress', '#message-input', addMessage);
