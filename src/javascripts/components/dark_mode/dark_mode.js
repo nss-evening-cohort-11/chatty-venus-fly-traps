88b25b0325976da64cdf9d60fd73579b871494f2
John Fleming <johndeclanfleming@gmail.com>
2020-03-14 12:02:36 -0500

    dark mode toggling

--- src/javascripts/components/dark_mode/dark_mode.js
+++ src/javascripts/components/dark_mode/dark_mode.js
@@ -1,5 +1,17 @@
 const toggleDarkMode = () => {
-  $('body').toggleClass('dark-mode');
+  if ($('#dark-mode-checkbox').hasClass('unchecked')) {
+    $('#dark-mode-checkbox').toggleClass('checked');
+    $('#dark-mode-checkbox').removeClass('unchecked');
+    $('body').toggleClass('dark-mode');
+    $('#navbar').addClass('navbar-dark bg-dark');
+    $('#navbar').removeClass('navbar-light bg-light');
+  } else if ($('#dark-mode-checkbox').hasClass('checked')) {
+    $('#dark-mode-checkbox').toggleClass('unchecked');
+    $('#dark-mode-checkbox').removeClass('checked');
+    $('body').removeClass('dark-mode');
+    $('#navbar').removeClass('navbar-dark bg-dark');
+    $('#navbar').addClass('navbar-light bg-light');
+  }
 };
 
 export default { toggleDarkMode };
