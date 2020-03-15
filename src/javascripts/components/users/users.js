0d1628d055ec4452abaa82173e8b187ec60392b9
John Fleming <johndeclanfleming@gmail.com>
2020-03-15 16:04:47 -0500

    moved checkboxes into the navbar

--- src/javascripts/components/users/users.js
+++ src/javascripts/components/users/users.js
@@ -4,8 +4,8 @@ import userData from '../../helper/data/userData';
 const printUsers = () => {
   const allUsers = userData.getUsers();
   let domString = '';
-  domString += '<select class="custom-select" id="inputGroupSelect02">';
-  domString += '<option selected>Choose...</option>';
+  domString += '<select class="custom-select" id="inputGroupSelect">';
+  domString += '<option selected>Select User</option>';
   allUsers.forEach((user) => {
     domString += `<option class="dropdown-item" id="${user.id}" value="${user.name}">${user.name}</option>`;
   });
@@ -14,13 +14,13 @@ const printUsers = () => {
 };
 
 const selectName = () => {
-  const selectedUserId = $('#inputGroupSelect02 :selected').attr('id');
+  const selectedUserId = $('#inputGroupSelect :selected').attr('id');
   const users = userData.getUsers();
   const selectedIndex = users.findIndex((x) => x.id === selectedUserId);
   return users[selectedIndex].name;
 };
 const selectColor = () => {
-  const selectedUserId = $('#inputGroupSelect02 :selected').attr('id');
+  const selectedUserId = $('#inputGroupSelect :selected').attr('id');
   const users = userData.getUsers();
   const selectedIndex = users.findIndex((x) => x.id === selectedUserId);
   return users[selectedIndex].color;
